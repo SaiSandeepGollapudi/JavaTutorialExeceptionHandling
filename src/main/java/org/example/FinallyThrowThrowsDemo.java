5bb516b917c121dbd2bc63f3fc4dca7e6bde5227
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-03-20 17:36:52 -0400

    Java

---
+++ src/main/java/org/example/FinallyThrowThrowsDemo.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class FinallyThrowThrowsDemo {
+    static void meth1()throws Exception
+    {
+        try
+        {
+
+            throw new Exception();
+        }
+        finally
+        {
+            //this message will definitely execute
+            System.out.println("Final Message");
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        meth1();
+    }
+}
